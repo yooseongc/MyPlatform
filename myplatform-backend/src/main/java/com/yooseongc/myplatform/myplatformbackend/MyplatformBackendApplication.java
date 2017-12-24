d714fe67c7139bece212092da87da892f137c53f
Yooseong Choi <choiys3574@miraeclimate.com>
2017-12-24 16:18:24 +0900

    Initial Settings

---
+++ myplatform-backend/src/main/java/com/yooseongc/myplatform/myplatformbackend/MyplatformBackendApplication.java
@@ -0,0 +1,12 @@
+package com.yooseongc.myplatform.myplatformbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyplatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyplatformBackendApplication.class, args);
+	}
+}
